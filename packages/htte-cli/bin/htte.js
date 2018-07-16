ff6decf66b113bed80902539db46e52caf95c284
Sigoden Huang <sigoden@gmail.com>
2018-07-16 19:52:52 +0800

    fix htte-cli

--- packages/htte-cli/bin/htte.js
+++ packages/htte-cli/bin/htte.js
@@ -7,8 +7,6 @@ program
   .option('--bail', 'Specify whether or not to gracefully stop a htte run on encountering an error')
   .option('--continue', 'Specify whether or not to continue run from abort unit')
   .option('--patch', 'Specify a patch config file to override the options in default config file')
-  .option('--silent', 'Prevents newman from showing output to CLI.')
-  .option('--disable-unicode', 'Forces unicode compliant symbols to be replaced by their plain text equivalents')
   .parse(process.argv);
 
 let app;
